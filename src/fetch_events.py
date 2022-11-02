a4c9aa7258f93b667ba76fcd9310d14d31f713a2
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2022-11-01 21:06:36 -0400

    [change] rewrite fetch_events to use boto3

--- src/fetch_events.py
+++ src/fetch_events.py
@@ -1,133 +1,81 @@
-import shutil
-import subprocess
+import gzip
+import itertools
+import json
+from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
-from pathlib import Path
-from typing import Generator, List, Union, cast
+from typing import List, Tuple
 
 import pandas as pd
+from mypy_boto3_s3.service_resource import ObjectSummary, S3ServiceResource
+from mypy_boto3_s3.type_defs import GetObjectOutputTypeDef
 
 from src.helpers.datetime import convert_to_s3_folder
-from src.helpers.exceptions import EventFetchError
-from src.helpers.io import read_gzipped_json_records
+from src.helpers.exceptions import (
+    EventObjectDecompressError,
+    EventObjectFetchError,
+    EventObjectParseError,
+)
 
 
 def fetch_events(
-    site_bucket_name: str,
-    timestamps: List[datetime],
-    path_to_data_dir: Union[str, Path],
-    num_processes: int,
-    delete_after_read: bool = True,
-) -> Generator[pd.DataFrame, None, None]:
+    s3_resource: S3ServiceResource, site_bucket_name: str, timestamps: List[datetime], num_concurrent_downloads: int
+) -> pd.DataFrame:
     """
     Given config inputs, including a site's bucket name and a list of date-hour
     timestamps, fetches corresponding S3 Snowplow event files and returns them
-    as a generator of pandas DataFrames (instead of a list of DataFrames,
-    which uses more memory), each corresponding to one timestamp.
+    as a single DataFrame.
+
+    Requires an S3ServiceResource as a parameter; here's how to create it:
+    >>> import boto3
+    >>> s3_resource = boto3.resource("s3")
     """
-    processes: dict[datetime, subprocess.Popen] = {}
-    timestamps_to_download = iter(timestamps)
-    # Turn path_to_data_dir into a Path object if it's a string
-    path_to_data_dir = Path(path_to_data_dir)
+    # Grab S3 bucket
+    bucket = s3_resource.Bucket(site_bucket_name)
 
-    # Start fetching for the first num_processes timestamps
-    for _ in range(num_processes):
-        timestamp = next(timestamps_to_download, None)
-        if timestamp:
-            processes[timestamp] = _fetch_folder(site_bucket_name, timestamp, path_to_data_dir)
+    # Get a list of summaries of S3 objects to fetch
+    object_summaries_by_timestamp = [
+        bucket.objects.filter(Prefix=f"enriched/good/{convert_to_s3_folder(ts)}") for ts in timestamps
+    ]
+    object_summaries = itertools.chain(*object_summaries_by_timestamp)
 
-    # As soon as a process finishes and is freed up, save its data into a DataFrame and fetch the next timestamp
-    for i, timestamp in enumerate(timestamps):
-        process = processes[timestamp]
+    # Spread fetching tasks over a number of CPU threads. Until aioboto3 is thoroughly documented and isn't a pain
+    # to work with (or until boto3 is asyncio-friendly), multithreading is a decent alternative
+    # Fetching all data (even gzipped) from the get-go might incur significant memory footprint, but this is a simple start
+    with ThreadPoolExecutor(max_workers=num_concurrent_downloads) as executor:
+        dfs = executor.map(_fetch_decompress_parse, object_summaries)
 
-        # Wait for fetch to finish using Popen.communicate
-        _, process_error_message = process.communicate()
-        # Raise exception if return code is not 0, indicating an error happened
-        process_return_code = process.returncode
-        process_args = cast(List[str], process.args)  # to appease mypy
-        if process_return_code != 0:
-            raise EventFetchError(
-                f"Subprocess command failed with return code {process_return_code}. "
-                + f"MESSAGE: {process_error_message.decode('utf-8').strip()}. "
-                + f"COMMAND: {' '.join(process_args)}."
-            )
-
-        # Start next fetch job in the queue
-        timestamp_next = next(timestamps_to_download, None)
-        if timestamp_next:
-            processes[timestamp_next] = _fetch_folder(site_bucket_name, timestamp_next, path_to_data_dir)
-
-        # Read fetched data into a DataFrame
-        df = _read_folder(site_bucket_name, timestamp, path_to_data_dir)
-
-        # Delete local data folder from disk
-        if delete_after_read:
-            if i == len(timestamps) - 1:
-                # Once reach the end of timestamp list, delete entire data directory
-                # containing all local folders to remove clutter
-                shutil.rmtree(path_to_data_dir)
-            else:
-                # Remove S3-downloaded folder we just read from
-                _delete_folder(site_bucket_name, timestamp, path_to_data_dir)
-
-        # Yield the fetched data for next step, i.e., transformation
-        yield df
-
-
-def _fetch_folder(site_bucket_name: str, timestamp: datetime, path_to_data_dir: Path) -> subprocess.Popen:
-    """
-    Runs a command downloading an S3 folder of enriched Snowplow events in a site's
-    bucket corresponding to a given date and hour.
-    """
-    timestamp_folder = convert_to_s3_folder(timestamp)
-
-    path_local = path_to_data_dir / timestamp_folder
-    # Create the path directory. It shouldn't have already existed because it's
-    # deleted at the end of a job run, but setting exist_ok=True prevents error if
-    # it already exists
-    path_local.mkdir(parents=True, exist_ok=True)
-
-    # Probably fine to put the "enriched/good" string here since we're only using enriched data,
-    # it can be passed to as a function parameter if needed
-    # TODO: If Site is a full-fledged class, probably still worth passing just a bucket name,
-    # e.g., site_bucket_name = Site.s3_bucket_name instead of passing the entire Site instance
-    path_s3 = Path(site_bucket_name) / "enriched/good" / timestamp_folder
-    uri_s3 = f"s3://{path_s3}"
-
-    command = f"aws s3 sync {uri_s3} {path_local}"
-
-    # TODO: Once a global logging config is set, log the exact command to the console
-    # Would also be helpful to customize log message by adding file name, function & line
-    # number of where the log happens, kind of like this:
-    # https://github.com/LocalAtBrown/experiment-semantic-similarity/blob/main/notebooks/util/logging.py
-
-    # Run the command
-    return subprocess.Popen(command.split(" "), stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
-
-
-def _read_folder(site_bucket_name: str, timestamp: datetime, path_to_data_dir: Path) -> pd.DataFrame:
-    """
-    Opens all .gz files in a local data folder given its corresponding site bucket
-    name and date-hour timestamp and combine all their data into a single pandas DataFrame.
-    """
-    # Local directory holding all data files fetched from the remote S3 folder
-    path_local = path_to_data_dir / convert_to_s3_folder(timestamp)
-
-    # Probably only need *.gz instead of **/*.gz because there should be no directory
-    # within the local data folder, but using the latter for good measure
-    dfs = [read_gzipped_json_records(path_file) for path_file in path_local.glob("**/*.gz")]
-
-    # If there are no files/DataFrames, return an empty DataFrame, since pd.concat([]) throws an error
-    if len(dfs) == 0:
-        # TODO: logging.info showing no data available to fetch for this particular date-hour timestamp
-        return pd.DataFrame()
-
-    # Combine into a single DataFrame
     return pd.concat(dfs)
 
 
-def _delete_folder(site_bucket_name: str, timestamp: datetime, path_to_data_dir: Path) -> None:
-    """
-    Deletes an S3-fetched folder corresponding to the given site bucket name and date-hour timestamp.
-    """
-    path_local = path_to_data_dir / convert_to_s3_folder(timestamp)
-    shutil.rmtree(path_local)
+def _fetch_decompress_parse(object_summary: ObjectSummary) -> pd.DataFrame:
+    key, response = _fetch_object(object_summary)
+    data = _decompress_object(key, response)
+    df = _parse_object(key, data)
+    return df
+
+
+def _fetch_object(object_summary: ObjectSummary) -> Tuple[str, GetObjectOutputTypeDef]:
+    key = object_summary.key
+    try:
+        response = object_summary.get()
+    except Exception as e:
+        raise EventObjectFetchError(f"Failed to fetch object {object_summary.key}") from e
+    return key, response
+
+
+def _decompress_object(object_key: str, object_response: GetObjectOutputTypeDef) -> bytes:
+    try:
+        data = object_response["Body"].read()
+        data = gzip.decompress(data)
+    except Exception as e:
+        raise EventObjectDecompressError(f"Failed to decompress/unzip object {object_key}") from e
+    return data
+
+
+def _parse_object(object_key: str, object_data: bytes) -> pd.DataFrame:
+    try:
+        data = object_data.decode("utf-8").strip().split("\n")
+        data = [json.loads(row) for row in data]
+    except Exception as e:
+        raise EventObjectParseError(f"Failed to parse object {object_key} into a DataFrame") from e
+    return pd.DataFrame.from_records(data)
--- src/helpers/exceptions.py
+++ src/helpers/exceptions.py
@@ -1,6 +1,26 @@
-class EventFetchError(Exception):
+# Snowplow fetching
+class EventObjectFetchError(Exception):
     """
-    Custom exception upon failure during the Snowplow-fetching process.
+    Custom exception upon failure while fetching an S3 object.
     """
 
     pass
+
+
+class EventObjectDecompressError(Exception):
+    """
+    Custon exception upon failure while decompressing/unzipping an S3 object.
+    """
+
+    pass
+
+
+class EventObjectParseError(Exception):
+    """
+    Custom exception upon failure while parsing the contents of an S3 object.
+    """
+
+
+# Snowplow transformation
+
+# Snowplow upload to Redshift
