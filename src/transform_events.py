7d6095d82186a9e8e13812eda5ffcfb0877d63e4
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2022-11-08 16:02:40 -0500

    [add] transform_fields

--- src/transform_events.py
+++ src/transform_events.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from typing import Dict, Set
+from typing import Set
 
 import pandas as pd
 
@@ -10,73 +10,109 @@ class EventFields(Enum):
     Snowplow documentation of these fields can be found here: https://docs.snowplow.io/docs/understanding-your-pipeline/canonical-event/.
     """
 
-    # Timestamp making allowance for innaccurate device clock
+    # [DATETIME] Timestamp making allowance for innaccurate device clock
     DERIVED_TSTAMP = "derived_tstamp"
 
-    # The page's height in pixels
+    # [FLOAT] The page's height in pixels
     DOC_HEIGHT = "doc_height"
 
-    # Number of the current user session, e.g. first session is 1, next session is 2, etc. Dependent on domain_userid
+    # [INT] Number of the current user session, e.g. first session is 1, next session is 2, etc. Dependent on domain_userid
     DOMAIN_SESSIONIDX = "domain_sessionidx"
 
-    # User ID set by Snowplow using 1st party cookie. Our primary identification key
+    # [STR, CATEGORICAL if needed] User ID set by Snowplow using 1st party cookie. Our primary identification key
     DOMAIN_USERID = "domain_userid"
 
-    # Screen height in pixels. Almost 1-to-1 relationship with domain_userid (there are exceptions)
+    # [FLOAT] Screen height in pixels. Almost 1-to-1 relationship with domain_userid (there are exceptions)
     DVCE_SCREENHEIGHT = "dvce_screenheight"
 
-    # Screen width in pixels. Almost 1-to-1 relationship with domain_userid (there are exceptions)
+    # [FLOAT] Screen width in pixels. Almost 1-to-1 relationship with domain_userid (there are exceptions)
     DVCE_SCREENWIDTH = "dvce_screenwidth"
 
-    # Name of event. Can be "page_view", "page_ping", "focus_form", "change_form", "submit_form"
+    # [STR, CATEGORICAL] Name of event. Can be "page_view", "page_ping", "focus_form", "change_form", "submit_form"
     EVENT_NAME = "event_name"
 
-    # User ID set by Snowplow using 3rd party cookie
+    # [STR, CATEGORICAL if needed] User ID set by Snowplow using 3rd party cookie
     NETWORK_USERID = "network_userid"
 
-    # Path to page, e.g., /event-directory/ in https://dallasfreepress.com/event-directory/
+    # [STR, CATEGORICAL if needed] Path to page, e.g., /event-directory/ in https://dallasfreepress.com/event-directory/
     PAGE_URLPATH = "page_urlpath"
 
-    # URL of the referrer
+    # [STR] URL of the referrer
     PAGE_REFERRER = "page_referrer"
 
-    # Maximum page y-offset seen in the last ping period. Depends on event_name == "page_ping"
+    # [FLOAT] Maximum page y-offset seen in the last ping period. Depends on event_name == "page_ping"
     PP_YOFFSET_MAX = "pp_yoffset_max"
 
-    # Type of referer. Can be "social", "search", "internal", "unknown", "email" (read: https://docs.snowplow.io/docs/enriching-your-data/available-enrichments/referrer-parser-enrichment/)
+    # [STR, CATEGORICAL] Type of referer. Can be "social", "search", "internal", "unknown", "email"
+    # (read: https://docs.snowplow.io/docs/enriching-your-data/available-enrichments/referrer-parser-enrichment/)
     REFR_MEDIUM = "refr_medium"
 
-    # Name of referer if recognised, e.g., "Google" or "Bing"
+    # [STR, CATEGORICAL] Name of referer if recognised, e.g., "Google" or "Bing"
     REFR_SOURCE = "refr_source"
 
-    # Data/attributes of HTML input and its form in JSON format. Only present if event_name == "change_form"
+    # [STR (JSON)] Data/attributes of HTML input and its form in JSON format. Only present if event_name == "change_form"
     # (read: https://github.com/snowplow/iglu-central/blob/master/schemas/com.snowplowanalytics.snowplow/change_form/jsonschema/1-0-0)
     UNSTRUCT_FORM_CHANGE = "unstruct_event_com_snowplowanalytics_snowplow_change_form_1"
 
-    # Data/attributes of HTML input and its form in JSON format. Only present if event_name == "focus_form"
+    # [STR (JSON)] Data/attributes of HTML input and its form in JSON format. Only present if event_name == "focus_form"
     # (read: https://github.com/snowplow/iglu-central/blob/master/schemas/com.snowplowanalytics.snowplow/focus_form/jsonschema/1-0-0)
     UNSTRUCT_FORM_FOCUS = "unstruct_event_com_snowplowanalytics_snowplow_focus_form_1"
 
-    # Data/attributes of HTML form and all its inputs in JSON format. Only present if event_name == "submit_form"
+    # [STR (JSON)] Data/attributes of HTML form and all its inputs in JSON format. Only present if event_name == "submit_form"
     # (read: https://github.com/snowplow/iglu-central/blob/master/schemas/com.snowplowanalytics.snowplow/submit_form/jsonschema/1-0-0)
     UNSTRUCT_FORM_SUBMIT = "unstruct_event_com_snowplowanalytics_snowplow_submit_form_1"
 
-    # Raw useragent
+    # [STR] Raw useragent
     USERAGENT = "useragent"
 
 
-def delete_rows(df: pd.DataFrame, fields_required: Set[EventFields]) -> pd.DataFrame:
-    df = df.dropna(subset=[field.value for field in fields_required])
+def delete_rows_and_fields(
+    df: pd.DataFrame, fields_to_keep: Set[EventFields], fields_required: Set[EventFields]
+) -> pd.DataFrame:
+    """
+    Cleans Snowplow events DataFrame.
+    """
+    # Remove unncessary fields
+    df = df[[f.value for f in fields_to_keep]]
+    # Some fields, e.g., derived_tstamp, shouldn't be empty under any circumstance,
+    # so we drop rows where these fields are empty
+    df = df.dropna(subset=[f.value for f in fields_required])
+    # Remove duplicate rows
     df = df.drop_duplicates()
 
+    # TODO: Maybe do something to weed out form events whose unstruct data, e.g., the HTML form class name, says it's not a newsletter form?
 
-def transform_fields(df: pd.DataFrame) -> pd.DataFrame:
+
+def transform_fields(
+    df: pd.DataFrame,
+    fields_int: Set[EventFields],
+    fields_float: Set[EventFields],
+    fields_datetime: Set[EventFields],
+    fields_categorical: Set[EventFields],
+) -> pd.DataFrame:
+    """
+    Performs transformations (not including aggregation) on a Snowplow events DataFrame.
+    """
+    df = df.copy()  # To protect original DataFrame's integrity, but can be removed if memory footprint is an issue
+
+    fields_int = [f.value for f in fields_int]
+    fields_float = [f.value for f in fields_float]
+    fields_datetime = [f.value for f in fields_datetime]
+    fields_categorical = [f.value for f in fields_categorical]
+
+    df[fields_int] = df[fields_int].astype(int)
+    df[fields_float] = df[fields_float].astype(float)
+    # pd.to_datetime can only turn pandas Series to datetime
+    for field in fields_datetime:
+        df[field] = pd.to_datetime(df[field])
+    df[fields_categorical] = df[fields_categorical].astype("category")
+
     return df
 
 
-def add_fields(df: pd.DataFrame) -> pd.DataFrame:
-    return df
+# def add_fields(df: pd.DataFrame) -> pd.DataFrame:
+#     return df
 
 
-def split_into_tables(df: pd.DataFrame) -> Dict[str, pd.DataFrame]:
-    return {"hi": df}
+# def split_into_tables(df: pd.DataFrame) -> Dict[str, pd.DataFrame]:
+#     return {"hi": df}
