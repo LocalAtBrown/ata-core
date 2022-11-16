1a77997aeac3a535da8c2d2ee853489c6a8342a5
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2022-11-16 13:15:07 -0500

    Preprocess Snowplow events (#8)
    * [add] relevant Snowplow fields
    
    * [add] function skeletons
    
    * [add] transform_fields
    
    * [change] cleanup
    
    * [change] better field names and comments
    
    * [change] better func name
    
    * [change] comment
    
    * [change] comment
    
    * [remove] comment out df dedupe
    
    * [add] comments
    
    * [change] better fn name
    
    * [change] comments
    
    * [change] better module name
    
    * [change] split delete function and shorten enum name
    
    * [add] event ID dedupe func
    
    * [remove] comments
    
    * [add] main preprocess function
    
    * [add] comment
    
    * [change] comment
    
    * [add] unit tests
    
    * [change] appease mypy
    
    * [add] timestamp conversion to UTC
    
    * [add] more test assertions
    
    * [change] comment
    
    * [change] comment
    
    * [change] have _delete_fields deal with nonexistent fields & change its name
    
    * [add] have Field subclass from str

---
+++ src/preprocess_events.py
@@ -0,0 +1,158 @@
+from enum import Enum
+from typing import Set
+
+import pandas as pd
+
+
+class Field(str, Enum):
+    """
+    Enum for Snowplow fields of interest.
+    Snowplow documentation of these fields can be found here: https://docs.snowplow.io/docs/understanding-your-pipeline/canonical-event/.
+
+    This class also subclasses from str so that, e.g., Field.DERIVED_TSTAMP == "derived_tstamp",
+    which means we can pass Field.DERIVED_TSTAMP into a pandas DataFrame directly without having
+    to grab its string value (Field.DERIVED_TSTAMP.value) first. (See use case as well as caveats:
+    https://stackoverflow.com/questions/58608361/string-based-enum-in-python.)
+    """
+
+    # [DATETIME] Timestamp making allowance for innaccurate device clock
+    DERIVED_TSTAMP = "derived_tstamp"
+
+    # [FLOAT] The page's height in pixels
+    DOC_HEIGHT = "doc_height"
+
+    # [INT] Number of the current user session, e.g. first session is 1, next session is 2, etc. Dependent on domain_userid
+    DOMAIN_SESSIONIDX = "domain_sessionidx"
+
+    # [STR, CATEGORICAL if needed] User ID set by Snowplow using 1st party cookie
+    DOMAIN_USERID = "domain_userid"
+
+    # [FLOAT] Screen height in pixels. Almost 1-to-1 relationship with domain_userid (there are exceptions)
+    DVCE_SCREENHEIGHT = "dvce_screenheight"
+
+    # [FLOAT] Screen width in pixels. Almost 1-to-1 relationship with domain_userid (there are exceptions)
+    DVCE_SCREENWIDTH = "dvce_screenwidth"
+
+    # [STR] ID of event. This would be the primary key within the site DataFrame,
+    # and part of the [site_name, event_id] composite key in the database table
+    EVENT_ID = "event_id"
+
+    # [STR, CATEGORICAL] Name of event. Can be "page_view", "page_ping", "focus_form", "change_form", "submit_form"
+    EVENT_NAME = "event_name"
+
+    # [STR, CATEGORICAL if needed] User ID set by Snowplow using 3rd party cookie
+    NETWORK_USERID = "network_userid"
+
+    # [STR, CATEGORICAL if needed] Path to page, e.g., /event-directory/ in https://dallasfreepress.com/event-directory/
+    PAGE_URLPATH = "page_urlpath"
+
+    # [STR] URL of the referrer
+    PAGE_REFERRER = "page_referrer"
+
+    # [FLOAT] Maximum page y-offset seen in the last ping period. Depends on event_name == "page_ping"
+    PP_YOFFSET_MAX = "pp_yoffset_max"
+
+    # [STR, CATEGORICAL] Type of referer. Can be "social", "search", "internal", "unknown", "email"
+    # (read: https://docs.snowplow.io/docs/enriching-your-data/available-enrichments/referrer-parser-enrichment/)
+    REFR_MEDIUM = "refr_medium"
+
+    # [STR, CATEGORICAL] Name of referer if recognised, e.g., "Google" or "Bing"
+    REFR_SOURCE = "refr_source"
+
+    # [STR (JSON)] Data/attributes of HTML input and its form in JSON format. Only present if event_name == "change_form"
+    # (read: https://github.com/snowplow/iglu-central/blob/master/schemas/com.snowplowanalytics.snowplow/change_form/jsonschema/1-0-0)
+    SEMISTRUCT_FORM_CHANGE = "unstruct_event_com_snowplowanalytics_snowplow_change_form_1"
+
+    # [STR (JSON)] Data/attributes of HTML input and its form in JSON format. Only present if event_name == "focus_form"
+    # (read: https://github.com/snowplow/iglu-central/blob/master/schemas/com.snowplowanalytics.snowplow/focus_form/jsonschema/1-0-0)
+    SEMISTRUCT_FORM_FOCUS = "unstruct_event_com_snowplowanalytics_snowplow_focus_form_1"
+
+    # [STR (JSON)] Data/attributes of HTML form and all its inputs in JSON format. Only present if event_name == "submit_form"
+    # (read: https://github.com/snowplow/iglu-central/blob/master/schemas/com.snowplowanalytics.snowplow/submit_form/jsonschema/1-0-0)
+    SEMISTRUCT_FORM_SUBMIT = "unstruct_event_com_snowplowanalytics_snowplow_submit_form_1"
+
+    # [STR] Raw useragent
+    USERAGENT = "useragent"
+
+
+def preprocess_events(
+    df: pd.DataFrame,
+    fields_relevant: Set[Field],
+    fields_required: Set[Field],
+    field_primary_key: Field,
+    fields_int: Set[Field],
+    fields_float: Set[Field],
+    fields_datetime: Set[Field],
+    fields_categorical: Set[Field],
+) -> pd.DataFrame:
+    """
+    Main Snowplow events DataFrame preprocessing function.
+    """
+    # TODO: After each step, if applicable, log how many rows or fields were deleted
+    df = _select_fields_relevant(df, fields_relevant)
+    df = _delete_rows_duplicate_key(df, field_primary_key)
+    df = _delete_rows_empty(df, fields_required)
+    df = _convert_field_types(df, fields_int, fields_float, fields_datetime, fields_categorical)
+
+    return df
+
+
+def _select_fields_relevant(df: pd.DataFrame, fields_relevant: Set[Field]) -> pd.DataFrame:
+    """
+    Select relevant fields from an events DataFrame. If a field doesn't exist,
+    it'll be added to the result DataFrame as an empty column.
+    """
+    # Sometimes, df doesn't have all the fields in fields_relevant, so we create
+    # an empty DataFrame with all the fields we'd like to have and concatenate df to it
+    df_empty_with_all_fields = pd.DataFrame(columns=[*fields_relevant])
+    # Get a list of fields in fields_relevant that are actually in df, because we
+    # don't want to query for nonexistent fields and have pandas raise a KeyError
+    fields_available = df.columns.intersection([*fields_relevant])
+
+    # Query for fields in fields_available and perform said concatenation, so that
+    # the final DataFrame will have all the fields in fields_relevant
+    return pd.concat([df_empty_with_all_fields, df[[*fields_available]]])
+
+
+def _delete_rows_empty(df: pd.DataFrame, fields_required: Set[Field]) -> pd.DataFrame:
+    """
+    Given a list of fields that cannot have empty or null data, remove all rows
+    with null values in any of these fields.
+    """
+    return df.dropna(subset=[*fields_required])
+
+
+def _delete_rows_duplicate_key(df: pd.DataFrame, field_primary_key: Field) -> pd.DataFrame:
+    """
+    Delete all rows whose primary key is repeated in the DataFrame.
+    """
+    return df.drop_duplicates(subset=[field_primary_key], keep=False)
+
+
+def _convert_field_types(
+    df: pd.DataFrame,
+    fields_int: Set[Field],
+    fields_float: Set[Field],
+    fields_datetime: Set[Field],
+    fields_categorical: Set[Field],
+) -> pd.DataFrame:
+    """
+    Changes data types in a Snowplow events DataFrame to those desired.
+    """
+    # Make a copy of the original so that it's not affected, but can remove
+    # this if memory is an issue
+    df = df.copy()
+
+    df[[*fields_int]] = df[[*fields_int]].astype(int)
+    df[[*fields_float]] = df[[*fields_float]].astype(float)
+
+    # pd.to_datetime can only turn pandas Series to datetime, so need to convert
+    # one Series/column at a time
+    # All timestamps should already be in UTC: https://discourse.snowplow.io/t/what-timezones-are-the-timestamps-set-in/622,
+    # but setting utc=True just to be safe
+    for field in fields_datetime:
+        df[field] = pd.to_datetime(df[field], utc=True)
+
+    df[[*fields_categorical]] = df[[*fields_categorical]].astype("category")
+
+    return df
---
+++ tests/test_preprocess_events.py
@@ -0,0 +1,119 @@
+from typing import Set
+
+import pandas as pd
+import pytest
+from pandas.api.types import (
+    is_categorical_dtype,
+    is_datetime64_ns_dtype,
+    is_float_dtype,
+    is_int64_dtype,
+)
+
+from src.preprocess_events import (
+    Field,
+    _convert_field_types,
+    _delete_rows_duplicate_key,
+    _delete_rows_empty,
+    _select_fields_relevant,
+)
+
+
+# ---------- FIXTURES ----------
+@pytest.fixture(scope="module")
+def df() -> pd.DataFrame:
+    """
+    Creates a dummy events DataFrame for testing purposes. It doesn't reflect
+    real data by any means.
+    """
+    return pd.DataFrame(
+        [
+            ["A0", "1", "1500", None, "2022-11-01T00:00:01.051Z", "page_ping"],
+            ["A0", "1", None, "400", "2022-11-02T00:00:01.051Z", "submit_form"],
+            ["B1", "2", "2000", "200", "2022-12-01T00:00:01.051Z", "focus_form"],
+            ["C2", "1", "1500", "400", None, None],
+        ],
+        columns=[
+            Field.EVENT_ID.value,
+            Field.DOMAIN_SESSIONIDX.value,
+            Field.DOC_HEIGHT.value,
+            Field.PP_YOFFSET_MAX.value,
+            Field.DERIVED_TSTAMP.value,
+            Field.EVENT_NAME.value,
+        ],
+    )
+
+
+@pytest.fixture(scope="module")
+def fields_relevant() -> Set[Field]:
+    return {Field.EVENT_ID, Field.DERIVED_TSTAMP, Field.PAGE_URLPATH}
+
+
+@pytest.fixture(scope="module")
+def fields_required() -> Set[Field]:
+    # Field.Event_NAME isn't included in the dummy DataFrame, but it should
+    # still be included in the output DataFrame as an empty column
+    return {Field.EVENT_ID, Field.DOC_HEIGHT, Field.DERIVED_TSTAMP, Field.EVENT_NAME}
+
+
+@pytest.fixture(scope="module")
+def field_primary_key() -> Field:
+    return Field.EVENT_ID
+
+
+@pytest.fixture(scope="module")
+def fields_int() -> Set[Field]:
+    return {Field.DOMAIN_SESSIONIDX}
+
+
+@pytest.fixture(scope="module")
+def fields_float() -> Set[Field]:
+    return {Field.DOC_HEIGHT, Field.PP_YOFFSET_MAX}
+
+
+@pytest.fixture(scope="module")
+def fields_datetime() -> Set[Field]:
+    return {Field.DERIVED_TSTAMP}
+
+
+@pytest.fixture(scope="module")
+def fields_categorical() -> Set[Field]:
+    return {Field.EVENT_NAME}
+
+
+# ---------- TESTS ----------
+def test__select_fields_relevant(df, fields_relevant) -> None:
+    df = _select_fields_relevant(df, fields_relevant)
+    assert set(df.columns) == fields_relevant
+
+
+def test__delete_rows_empty(df, fields_required) -> None:
+    df = _delete_rows_empty(df, fields_required)
+    # doc_height is not required, so the first row is off the hook
+    assert df.shape[0] == 2
+    # isna() should return False for all cells under required fields;
+    # these false values sum up to 0
+    assert df[[*fields_required]].isna().to_numpy().sum() == 0
+
+
+def test__delete_rows_duplicate_key(df, field_primary_key) -> None:
+    df = _delete_rows_duplicate_key(df, field_primary_key)
+    # First 2 rows should be removed
+    assert df.shape[0] == 2
+    # Check primary key uniqueness
+    assert df[field_primary_key].is_unique
+
+
+def test__convert_field_types(df, fields_int, fields_float, fields_datetime, fields_categorical) -> None:
+    df = _convert_field_types(df, fields_int, fields_float, fields_datetime, fields_categorical)
+
+    for f in fields_int:
+        assert is_int64_dtype(df[f])
+
+    for f in fields_float:
+        assert is_float_dtype(df[f])
+
+    for f in fields_datetime:
+        assert is_datetime64_ns_dtype(df[f])
+
+    for f in fields_categorical:
+        assert is_categorical_dtype(df[f])
