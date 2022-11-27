b93850849919c44f15d0a4f55c3da3cc9a369d71
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2022-11-26 22:41:07 -0500

    [change] comment

--- src/preprocess_events.py
+++ src/preprocess_events.py
@@ -172,7 +172,9 @@ class ConvertFieldTypes(Preprocessor):
 
 def preprocess_events(df: pd.DataFrame, preprocessors: List[Preprocessor]) -> pd.DataFrame:
     """
-    Main Snowplow events DataFrame preprocessing function.
+    Main Snowplow events DataFrame preprocessing function, taking in a list of
+    predefined preprocessors as above. Since the output DataFrame of one preprocessor
+    becomes the input DataFrame of the one after it, the order of preprocessors matters.
     """
     for preprocessor in preprocessors:
         df = preprocessor.transform(df)
