4a464390bc42bf62ce82ba4dfed96ff4b7c4f62c
Raaid <raaid@protonmail.com>
2022-12-08 09:06:24 -0500

    wrap site name extraction with enum call to guarantee it is part of the enum

--- ata_pipeline0/main.py
+++ ata_pipeline0/main.py
@@ -26,7 +26,7 @@ def handler(event, context):
     # Note: this is invoked by an event-driven, async method (s3 trigger) so the return value is discarded
     # see here for example event structure: https://docs.aws.amazon.com/AmazonS3/latest/userguide/notification-content-structure.html
     bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
-    site_name = re.findall("lnl-snowplow-(.*)", bucket_name)[0]
+    site_name = SiteName(re.findall("lnl-snowplow-(.*)", bucket_name)[0])
     object_key = event["Records"][0]["s3"]["object"]["key"]
     run_pipeline(site_name=site_name, object_key=object_key, concurrency=1)
 
