409274b9bc1ebcf9d8461358a0b771850531dec2
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2022-12-09 14:04:05 +0700

    [add] preprocessor to main

--- ata_pipeline0/helpers/preprocessors.py
+++ ata_pipeline0/helpers/preprocessors.py
@@ -123,7 +123,7 @@ class DeleteRowsBot(Preprocessor):
         return df[df[self.field_useragent].apply(lambda x: not ua.parse(x).is_bot)]
 
     def log_result(self, df_in: pd.DataFrame, df_out: pd.DataFrame) -> None:
-        logger.info(f"Deleted {df_in.shape[0] - df_out.shape[0]} rows whose event is made by a bot.")
+        logger.info(f"Deleted {df_in.shape[0] - df_out.shape[0]} rows whose event is made by a bot")
 
 
 @dataclass
