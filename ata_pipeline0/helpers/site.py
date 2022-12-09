12c8ae664aa74010718cb3c40ff1e67ebfc709ed
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2022-12-09 23:57:47 +0700

    [add] comments

--- ata_pipeline0/helpers/site.py
+++ ata_pipeline0/helpers/site.py
@@ -43,7 +43,7 @@ class Site(ABC):
     @functools.cache
     def parse_form_submit_data(data: dict) -> FormSubmitData:
         """
-        TODO docs
+        Creates a form-data dataclass from a corresponding dict.
         """
         return FormSubmitData(
             form_id=data["formId"], form_classes=data["formClasses"], elements=[FormElement(*e) for e in data["elements"]]
@@ -52,7 +52,7 @@ class Site(ABC):
     @staticmethod
     def verify_newsletter_form_submit_nonempty_data(event: pd.Series) -> bool:
         """
-        TODO docs
+        Checks if a form-submission event actually has form HTML data.
         """
         # Should only be either dict or None because we'll perform this check
         # after the ConvertFieldTypes and ReplaceNaNs preprocessors
@@ -61,17 +61,23 @@ class Site(ABC):
 
     def verify_newsletter_form_submit_has_email_input(self, event: pd.Series) -> bool:
         """
-        TODO docs
+        Checks if the HTML form of a form-submission event has an `<input type="email">`
+        element, which is the case in all of our partners' newsletter forms.
         """
         form_data = self.parse_form_submit_data(event[FieldSnowplow.SEMISTRUCT_FORM_SUBMIT])
         return any([e.node_name == "INPUT" and e.type == "email" for e in form_data.elements])
 
     @property
     def newsletter_form_submit_verifiers(self) -> List[Callable[[pd.Series], bool]]:
+        """
+        List of verifiers used to check if a form-submission event is of a newsletter form.
+        It's supposed (but not required) to be extended (or superseded) by child classes of `Site`.
+        """
         return [self.verify_newsletter_form_submit_nonempty_data, self.verify_newsletter_form_submit_has_email_input]
 
     def verify_newsletter_form_submit(self, event: pd.Series) -> bool:
         """
-        TODO docs
+        Checks if a form-submission event is of a newsletter form using a pre-specified
+        list of individual verifiers. If one verifier fails, it automatically fails.
         """
         return all([verify(event) for verify in self.newsletter_form_submit_verifiers])
