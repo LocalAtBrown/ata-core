87d06c8ae0095149be0c96e5a72f6ae328cf52d3
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2022-12-10 11:08:16 +0700

    [add] comments

--- ata_pipeline0/helpers/site.py
+++ ata_pipeline0/helpers/site.py
@@ -24,6 +24,13 @@ class SiteName(str, Enum):
 # ---------- SITE FORM-SUBMISSION EVENT UTILITIES ----------
 @dataclass
 class FormElement:
+    """
+    JSON data schema of an element as part of Snowplow form-submission event data
+    (see: https://github.com/snowplow/iglu-central/blob/master/schemas/com.snowplowanalytics.snowplow/submit_form/jsonschema/1-0-0)
+    """
+
+    # These constraints are looser than what the schema requires. If we need
+    # them to be stricter, consider pydantic.
     name: str
     node_name: str
     value: Optional[str] = None
@@ -32,6 +39,13 @@ class FormElement:
 
 @dataclass
 class FormSubmitData:
+    """
+    JSON data schema of a Snowplow form-submission event
+    (see: https://github.com/snowplow/iglu-central/blob/master/schemas/com.snowplowanalytics.snowplow/submit_form/jsonschema/1-0-0)
+    """
+
+    # These constraints are looser than what the schema requires. If we need
+    # them to be stricter, consider pydantic.
     form_id: str
     form_classes: List[str]
     elements: List[FormElement]
@@ -40,7 +54,7 @@ class FormSubmitData:
 @functools.cache
 def parse_form_submit_dict(data: dict) -> FormSubmitData:
     """
-    Creates a form-data dataclass from a corresponding dict.
+    Creates a dataclass from a corresponding `dict` of form-submission data.
     """
     return FormSubmitData(
         form_id=data["formId"], form_classes=data["formClasses"], elements=[FormElement(*e) for e in data["elements"]]
